9c78dbbfc32be2c9a490e70f133ac83a2dbc71ef
Elias Schramm <privat@eps-dev.de>
2022-03-25 21:49:20 +0100

    feat: Add Logs

---
+++ backend/tools/log.ts
@@ -0,0 +1,28 @@
+import moment from 'moment';
+import { env } from 'process';
+const LOG_PREFIX = 'EPS-URL';
+
+const CLI_LOGO =
+`
+###############################################
+
+ _____ ____  ____            _   _ ____  _
+| ____|  _ \\/ ___|          | | | |  _ \\| |
+|  _| | |_) \\___ \\   _____  | | | | |_) | |
+| |___|  __/ ___) | |_____| | |_| |  _ <| |___
+|_____|_|   |____/           \\___/|_| \\_\\_____|
+
+
+VERSION: 0.0.1                       25.03.2022
+
+###############################################
+`;
+
+export function log(message: any): void {
+    console.log(`${moment().format('hh:mm:ss')}: [${LOG_PREFIX}] ${message}`);
+}
+
+export function splashscreen(): void {
+    log(env.DATABASE_URL);
+    for(const line of CLI_LOGO.split('\n')) log(line);
+}
