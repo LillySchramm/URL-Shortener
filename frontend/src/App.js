b0bb58b48ff35a0b9f54ca2bd5048fe5cf87e9e1
Elias Schramm <privat@eps-dev.de>
2022-03-27 21:24:36 +0200

    feat: Add Frontend Version Notice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import AppFooter from './components/app-footer/App-footer';
 import AppHeader from './components/app-header/App-header';
 import AppUrlInput from './components/app-url-input/App-url-input';
+import AppVersion from './components/app-version/App-version';
 
 function App() {
-  return (
-    <div className="App">
-      <AppHeader></AppHeader>
-      <AppUrlInput></AppUrlInput>
-      <AppFooter></AppFooter>
-    </div>
-  );
+    return (
+        <div>
+            <div className="App">
+                <AppHeader></AppHeader>
+                <AppUrlInput></AppUrlInput>
+                <AppFooter></AppFooter>
+            </div>
+            <AppVersion></AppVersion>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/app-version/App-version.js
@@ -0,0 +1,25 @@
+import './App-version.css'
+import React, { version } from 'react';
+
+const API_URL = "https://eps-dev.net"
+
+class AppVersion extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { version: '' };
+    }
+
+    componentDidMount() {
+        fetch(`${API_URL}/stats`).then((result) => result.json().then((responseJson) => {
+            this.setState({ version: responseJson.version })
+        }))
+    }
+
+    render() {
+        return (
+            <p className="version">v{this.state.version}</p>
+        );
+    }
+}
+
+export default AppVersion;
