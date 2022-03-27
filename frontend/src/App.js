71041bf5b36388748f84df23ee3d596332708f94
Elias Schramm <privat@eps-dev.de>
2022-03-27 01:19:42 +0100

    feat: Make Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import AppFooter from './components/app-footer/App-footer';
+import AppHeader from './components/app-header/App-header';
+import AppUrlInput from './components/app-url-input/App-url-input';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader></AppHeader>
+      <AppUrlInput></AppUrlInput>
+      <AppFooter></AppFooter>
     </div>
   );
 }
---
+++ frontend/src/components/app-header/App-header.js
@@ -0,0 +1,11 @@
+import './App-header.css'
+
+function AppHeader() {
+    return (
+        <div className="App-header">
+            <h1>EPS-URL</h1>
+        </div>
+    );
+}
+
+export default AppHeader;
